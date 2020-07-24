6c9d685019fdd85a8aeaeee0cbbb82472563aac6
Ian Jackson <ijackson@chiark.greenend.org.uk>
2020-07-24 23:53:41 +0100

    Add test case for remove_dir_contents
    We check a few error behaviours too.
    
    Signed-off-by: Ian Jackson <ijackson@chiark.greenend.org.uk>

--- src/portable.rs
+++ src/portable.rs
@@ -18,3 +18,43 @@ pub fn remove_dir_contents<P: AsRef<Path>>(path: P) -> io::Result<()> {
     _remove_dir_contents(path)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use tempfile::TempDir;
+    use crate::remove_dir_all;
+    use crate::remove_dir_contents;
+    use std::fs::{self, File};
+    use std::io;
+
+    fn expect_failure<T>(k: io::ErrorKind, r: io::Result<T>) -> io::Result<()> {
+        match r {
+            Err(e) if e.kind() == k => Ok(()),
+            Err(e) => Err(e),
+            Ok(_) => Err(io::Error::new(
+                io::ErrorKind::Other,
+                "unexpected success".to_string(),
+            )),
+        }
+    }
+
+    #[test]
+    fn mkdir_rm() -> Result<(), io::Error> {
+        let tmp = TempDir::new()?;
+        let ours = tmp.path().join("t.mkdir");
+        let file = ours.join("file");
+        fs::create_dir(&ours)?;
+        File::create(&file)?;
+        File::open(&file)?;
+
+        expect_failure(io::ErrorKind::Other, remove_dir_contents(&file))?;
+
+        remove_dir_contents(&ours)?;
+        expect_failure(io::ErrorKind::NotFound, File::open(&file))?;
+
+        remove_dir_contents(&ours)?;
+        remove_dir_all(&ours)?;
+        expect_failure(io::ErrorKind::NotFound, remove_dir_contents(&ours))?;
+        Ok(())
+    }
+}
