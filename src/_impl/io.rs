e3470200b7782a651b3a25a561bed73cbc4591c5
Robert Collins <robertc@robertcollins.net>
2024-07-24 16:38:56 +0200

    Windows dep and code tidyup (#72)

--- src/_impl/io.rs
+++ src/_impl/io.rs
@@ -1,14 +1,20 @@
 //! Private trait to deal with OS variance
 
-use std::{fmt::Debug, fs::File, io, path::Path};
+#[cfg(not(windows))]
+use std::fmt::Debug;
+use std::{fs::File, io, path::Path};
 
 pub(crate) trait Io {
+    #[cfg(not(windows))]
     type UniqueIdentifier: PartialEq + Debug;
 
     fn duplicate_fd(f: &mut File) -> io::Result<File>;
 
     fn open_dir(p: &Path) -> io::Result<File>;
+
+    #[cfg(not(windows))]
     fn unique_identifier(d: &File) -> io::Result<Self::UniqueIdentifier>;
 
+    #[cfg(not(windows))]
     fn is_eloop(e: &io::Error) -> bool;
 }
